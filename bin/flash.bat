b085b3deb6b262c31c7984d0bc3a811776e025c9
Tim Moore <tim@youngmoores.com>
2023-08-15 19:32:06 -0700

    verbose on

--- bin/flash.bat
+++ bin/flash.bat
@@ -1,1 +1,1 @@
-..\bin\proploader -T -D chipver=P2 -D reset=cts -i 192.168.1.26 -r ..\src\swervebot.binary
+..\bin\proploader -v -T -D chipver=P2 -D reset=cts -i 192.168.1.26 -r ..\src\swervebot.binary
--- bin/term.bat
+++ bin/term.bat
@@ -1,1 +1,1 @@
-..\bin\proploader.exe -i 192.168.1.26 -T
+..\bin\proploader.exe -v -i 192.168.1.26 -T
